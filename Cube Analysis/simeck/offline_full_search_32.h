44ad1e8da9bf7d22b6077044f371060a38099c03
mj5817 <mjz.i88t@yahoo.com>
2017-12-07 02:28:02 +0330

    v.0.0.0

--- Cube Analysis/simeck/offline_full_search_32.h
+++
@@ -1,6 +0,0 @@
-#ifndef OFFLINE_FULL_SEARCH_H__
-#define OFFLINE_FULL_SEARCH_H__
-
-void offline_full_search(int r);
-
-#endif
--- Cube Analysis/simeck/offline_search_32.h
+++
@@ -1,6 +0,0 @@
-#ifndef OFFLINE_SEARCH_H__
-#define OFFLINE_SEARCH_H__
-
-void offline_search(int r);
-
-#endif
